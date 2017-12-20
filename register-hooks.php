fa4cc1716dda7088859c6d6f2a278af2d71321bf
Georg Obermayr <georgobermayr@Georgs-MacBook-Pro.local>
2017-12-20 16:18:24 +0100

    FEAT: Ensure that revisions are only created on pages that are part of the editorial workflow

--- register-hooks.php
+++ register-hooks.php
@@ -2,15 +2,23 @@
 use JensTornell\Revisions as Revisions;
 
 kirby()->hook('panel.page.create', function( $page ) {
-	$obj = new Revisions\CreateRevisions( $page );
-	$obj = new Revisions\CreateRevision();
-	$obj->go( $page, 'create', $page->content()->language() );
+  // Bind the hook to the author field, if author field
+  // is present, wo know its part of the editorial workflow
+  if ( $page->content()->has('author') ) {
+    $obj = new Revisions\CreateRevisions( $page );
+    $obj = new Revisions\CreateRevision();
+    $obj->go( $page, 'create', $page->content()->language() );
+  };
 });
 
 kirby()->hook('panel.page.update', function( $page ) {
-	$obj = new Revisions\CreateRevisions( $page );
-	$obj = new Revisions\CreateRevision();
-	$obj->go( $page, 'update', $page->content()->language() );
+  // Bind the hook to the author field, if author field
+  // is present, wo know its part of the editorial workflow
+  if ( $page->content()->has('author') ) {
+    $obj = new Revisions\CreateRevisions( $page );
+    $obj = new Revisions\CreateRevision();
+    $obj->go( $page, 'update', $page->content()->language() );
+  };
 });
 
 function debug_hook($content) {
